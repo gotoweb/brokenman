48f3478cf1d66edb090680e3b63b564af5d9aba0
Hoyong Lee <igotoweb@gmail.com>
2023-01-21 18:58:20 +0900

    update app.js

--- app.js
+++ app.js
@@ -5,29 +5,28 @@ const app = express()
 app.use(cors())
 const port = 80
 
-let requestCount = 0
+// let requestCount = 0
 
 app.get('/', (req, res) => {
   console.log('[v3] running in pod ' + req.socket.remoteAddress)
-  requestCount = requestCount + 1
-  if(requestCount >= 5) {
-    console.log('[v3] internal server error')
-    res.status(500).send(`
-      <h1>Internal Server Error</h1>
-      <div>뭔가 문제가 있어요! 문제가 생긴 파드 이름: ${os.hostname()}</div>
-    `)
-    return;
-  }
-  else {
-    res.send(`
-      <h1>뭔가 수상한 CozServer에 오신 것을 환영합니다!</h1>
-      <div>버전:
-        <span style="color:red; font-weight: bold">v3.0</span>
-      </div>
-      <div>파드 이름: ${os.hostname()}</div>
-      <div>${req.socket.remoteAddress}에서 오셨군요!</div>
-    `)
-  }
+  res.send(`
+    <h1>뭔가 수상한 CozServer에 오신 것을 환영합니다!</h1>
+    <div>버전:
+      <span style="color:red; font-weight: bold">v3.0</span>
+    </div>
+    <div>파드 이름: ${os.hostname()}</div>
+    <div>${req.socket.remoteAddress}에서 오셨군요!</div>
+  `)
+})
+
+app.get('/wrong', (req, res) => {
+  console.log('[v3] running in pod ' + req.socket.remoteAddress)
+  console.log('[v3] internal server error')
+  res.status(500).send(`
+    <h1>Internal Server Error</h1>
+    <div>뭔가 문제가 있어요! 문제가 생긴 파드 이름: ${os.hostname()}</div>
+  `)
+  return;
 })
 
 app.listen(port, () => {
